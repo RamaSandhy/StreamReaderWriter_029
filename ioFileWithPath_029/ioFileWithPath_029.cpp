f1b91f1c9f288baace618b98f613555f8e10f61e
RamaSandhy <rshandy07@gmail.com>
2023-06-21 15:38:00 +0700

    melemparkan  sebuah interger  makan blok ini akan dieksekusi

--- ioFileWithPath_029/ioFileWithPath_029.cpp
+++ ioFileWithPath_029/ioFileWithPath_029.cpp
@@ -16,6 +16,7 @@ int main() {
 	outfile.open(NamaFile + ".txt", ios::out);
 
 	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
 	//unlimited loop untuk menulis
 	while (true) {
 		cout << "- ";
---
+++ manualException_029/manualException_029.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int mainn()
+{
+	try {
+		cout << "Selamat Belajar di Prodi TI UMY" << endl;
+		throw 0.5; // melemparkan sebuah interger maka
+		cout << "Pernyataan tidak akan dieksekusi" << endl;
+	}
+	catch (int a) {
+		//blok ini akan dieksekusi
+		cout << "Pengecualian akan dieksekusi" << endl;
+	}
+
+}
