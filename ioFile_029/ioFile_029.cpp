f1819fe5c51bf7749509ba87fc25d9b088765bbe
RamaSandhy <rshandy07@gmail.com>
2023-06-21 15:06:27 +0700

    melakukan perulangan setiap  baris, tampilkan di sini, dan tutup file tersebut setelah selesai

--- ioFile_029/ioFile_029.cpp
+++ ioFile_029/ioFile_029.cpp
@@ -32,4 +32,15 @@ int main() {
 
 	cout << endl << ">= Membuka dan membaca file " << endl;
 	//jika file ada maka
-}  
+	if (infile.is_open())
+	{
+		//melakukan perulangan setiap baris 
+		while (getline(infile, baris))
+		{
+			//dan tampilkan disini
+			cout << baris << '\n';
+		}
+		//tutup file tersebut setelah selesai
+
+	}
+}   
