c7717998a0cef1a28893fbd13e717fecc5c11a77
RamaSandhy <rshandy07@gmail.com>
2023-06-21 15:07:40 +0700

    jika tidak ditemukan file maka akan menampilkan ini

--- ioFile_029/ioFile_029.cpp
+++ ioFile_029/ioFile_029.cpp
@@ -41,6 +41,9 @@ int main() {
 			cout << baris << '\n';
 		}
 		//tutup file tersebut setelah selesai
-
+		infile.close();
 	}
+	//jika tidak ditemukan file maka akan menampilkan ini
+	else cout << "Unable to open file";
+	return 0;
 }   
